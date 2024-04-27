ea7cfa22be8cf42b5222115abf1e11e06feef429
Anugrahas2001 <anugrahas2001@gmail.com>
2024-04-27 16:35:01 +0530

    minor changes

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,10 +1,8 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Banner from "./Banner";
 import Header from "./Header";
 import Specialdishes from "./Specialdishes";
 import Filtereddishes from "./Filtereddishes";
-import Loader from "./Loader";
 import Footer from "./Footer";
 import AllMenu from "./AllMenuContext";
 
