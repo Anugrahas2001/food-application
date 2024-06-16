0d979f1258db2c140a7de0f463324aea3ce27cf5
Anugraha S <anugraha.s@fireflink.com>
2024-06-16 13:08:54 +0530

    Cart items added to the local storage

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -5,18 +5,20 @@ const StateContext=createContext();
 const AppProvider = ({children}) => {
 
    const initialState={
-    cart:[]
+    cart: JSON.parse(localStorage.getItem('cart')) || []
    }
    
    const reducer = (state, action) => {
     console.log(state);
     switch (action.type) {
       case "add_To_cart":
+        localStorage.setItem('cart', JSON.stringify([...state.cart, { title: action.payload.title, img: action.payload.img }]));
         return {
           ...state,
           cart: [...state.cart, { title: action.payload.title, img: action.payload.img }]
         };
       case "remove_From_cart":
+        localStorage.setItem('cart', JSON.stringify(state.cart.filter(item => item.title !== action.payload)));
         return {
           ...state,
           cart: state.cart.filter(item => item.title !== action.payload)
